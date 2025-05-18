80d18b12b81d526ff0e8729e98b1cf8d1587f6c8
Omar Faruq <omarfaruq9723@gmail.com>
2025-05-19 02:10:33 +0600

    frontend security issue solve then datapass

---
+++ JobkhujiBD/src/app/service/job-listing.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobListingService {
+  private baseUel = 'http://localhost:8080/api/job-listings';
+
+  constructor(private http: HttpClient) { }
+
+  createNewJob(job: any) {
+    return this.http.post(`${this.baseUel}`, job, { withCredentials: true });
+  }
+
+  getAllJobs() {
+    return this.http.get(`${this.baseUel}`, { withCredentials: true });
+  }
+
+  updateJob(jobId: number, job: any) {
+    return this.http.put(`${this.baseUel}/${jobId}`, job, { withCredentials: true });
+  }
+
+  deleteJob(jobId: number) {
+    return this.http.delete(`${this.baseUel}/${jobId}`, { withCredentials: true });
+  }
+
+  getAllCategory() {
+    return this.http.get('http://localhost:8080/api/categories', { withCredentials: true });
+  }
+}
